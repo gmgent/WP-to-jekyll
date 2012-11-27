690f3e5582de83f130ae98fa2130c38bf91dcabb
Krister Axel <axel@theta>
2012-11-27 02:31:07 -0800

    first successful search and the dragon is slain

--- blog/_site/scripts/ksearch.js
+++ blog/_site/scripts/ksearch.js
@@ -1,17 +1,36 @@
-var descList = new Array();
-var itemList = "";
-
-function messageLoading(el) {
-	document.getElementById(el).innerHTML = '<small>loading...</small>'; 
-}
-
 function run_search(term) {
 		
     $("#results").html("loading");
 
 	$.getJSON("/search.json", function(json) { 
-		console.log(json);
-		$("#results").html('<a href="/' + json[0].href + '">' + json[0].href + '</a>');
+		/* console.log(json); */
+		/* toLowerCase() */
+		
+		
+		$.each(json, function(index, item) {
+			if (item) {
+				
+				if (match_term(item, term)) {
+				  $("#items").append(make_link(item));	
+				};
+				
+			  };
+		        
+		    });
+		
 	} );
+	
+	$("#results").html("");
 
+}
+
+
+function make_link(it) {
+	return '<h3><a href="/' + it.href + '">' + it.title + '</a></h3>';
+}
+
+function match_term(init, searchterm) {
+	var instring = init.content.toLowerCase();
+	var compare = searchterm.toLowerCase();
+	return ( instring.search(compare) >= 0 );
 }
