0dbdd68616a32dd402688d2f95d8a10808a2ea43
Krister Axel <axel@theta>
2012-11-26 16:53:57 -0800

    properly adding search term

--- blog/_site/scripts/ksearch.js
+++ blog/_site/scripts/ksearch.js
@@ -4,8 +4,8 @@ function messageLoading(el) {
 	document.getElementById(el).innerHTML = '<small>loading...</small>'; 
 }
 
-function run_search() {
+function run_search(term) {
 	
-  $('#results').html("WE R WINNING");
+  $('#results').html("WE R WINNING " + term);
   callback()
 }
