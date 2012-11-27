ec27fb53bc173b19a92bcd88dfd19782641e44cc
Krister Axel <axel@theta>
2012-11-27 01:35:57 -0800

    there is light at the end of the tunnel - I am finally parsing json now i need to figure how to regex properly and we are done.

--- blog/_site/scripts/ksearch.js
+++ blog/_site/scripts/ksearch.js
@@ -1,25 +1,17 @@
-/* added */
+var descList = new Array();
+var itemList = "";
 
 function messageLoading(el) {
 	document.getElementById(el).innerHTML = '<small>loading...</small>'; 
 }
 
 function run_search(term) {
-	
-  	var content = "";
-    $("#results").html("PLEASE ME");
+		
+    $("#results").html("loading");
 
-	  $.getJSON("/search.json",
-	    function(json){
-	      console.log(json);
-		  
-	    }
-	  );
-
-
-
+	$.getJSON("/search.json", function(json) { 
+		console.log(json);
+		$("#results").html('<a href="/' + json[0].href + '">' + json[0].href + '</a>');
+	} );
 
 }
-
-
-      
