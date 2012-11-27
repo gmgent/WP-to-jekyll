e662c1c5fe25e406347bab01b5d4caebea32000a
Krister Axel <axel@theta>
2012-11-27 02:46:05 -0800

    search is pretty much nailed now.

--- blog/_site/scripts/ksearch.js
+++ blog/_site/scripts/ksearch.js
@@ -1,17 +1,21 @@
 function run_search(term) {
-		
+			
     $("#results").html("loading");
+    $("#items").html(""); 
 
 	$.getJSON("/search.json", function(json) { 
 		/* console.log(json); */
 		/* toLowerCase() */
 		
 		
+		$("#results").html("no match was found.");
 		$.each(json, function(index, item) {
 			if (item) {
 				
 				if (match_term(item, term)) {
-				  $("#items").append(make_link(item));	
+				  $("#items").append(make_link(item));
+				/*erase old message*/
+				  $("#results").html("");	
 				};
 				
 			  };
@@ -20,7 +24,6 @@ function run_search(term) {
 		
 	} );
 	
-	$("#results").html("");
 
 }
 
@@ -32,5 +35,13 @@ function make_link(it) {
 function match_term(init, searchterm) {
 	var instring = init.content.toLowerCase();
 	var compare = searchterm.toLowerCase();
-	return ( instring.search(compare) >= 0 );
+	
+	if ( instring.search(compare) >= 0 ) {
+		return true;
+	}
+	else {
+		var instring = init.title.toLowerCase();
+		return ( instring.search(compare) >= 0 );
+	}
+	
 }
