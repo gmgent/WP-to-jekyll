23c31f641fb91629082e1d672d3f8de5f583ba7b
Krister Axel <axel@theta>
2012-11-28 22:13:23 -0800

    ok rsync is configured correctly now | auto deploy 1354169603

--- deploy_command.rb
+++ deploy_command.rb
@@ -6,6 +6,5 @@ sudo rsync --delete --stats -tiprogl --progress /Users/theta/src/jekyll/blog/_si
 #sync assets from kristeraxel.com/media
 sudo rsync --delete --stats -tiprogl --progress /Users/theta/src/kristeraxel.com/media/ -e "ssh -i /Users/theta/src/aws/key/BlogKAkey.pem" ec2-user@107.22.165.75:/var/www/kristeraxel.com/media/
 
-#We will try this one next. TRYING AGAIN 2
 # sync assets from blog.kristeraxel.com/media
-sudo rsync --stats -tiprogl --progress /Users/theta/src/kristeraxel.com/wp-mirror/wp-content/ -e "ssh -i /Users/theta/src/aws/key/BlogKAkey.pem" ec2-user@107.22.165.75:/var/www/html/wp-content/
+sudo rsync --delete --stats -tiprogl --progress /Users/theta/src/kristeraxel.com/wp-mirror/wp-content/ -e "ssh -i /Users/theta/src/aws/key/BlogKAkey.pem" ec2-user@107.22.165.75:/var/www/html/wp-content/
