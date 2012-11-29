74ec588d13b61df9c2d54a55d78bb3dd5691ac78
Krister Axel <axel@theta>
2012-11-28 20:57:10 -0800

    added mor epushed for media through rsync | auto deploy 1354165030

--- deploy_command.rb
+++ deploy_command.rb
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 
+#sync main jekyll project 
 sudo rsync --delete --stats -tiprogl --progress /Users/theta/src/jekyll/blog/_site/ -e "ssh -i /Users/theta/src/aws/key/BlogKAkey.pem" ec2-user@107.22.165.75:/var/www/html/
+
+#sync assets from kristeraxel.com/media
+sudo rsync --delete --stats -tiprogl --progress /Users/theta/src/kristeraxel.com/media/ -e "ssh -i /Users/theta/src/aws/key/BlogKAkey.pem" ec2-user@107.22.165.75:/var/www/kristeraxel.com/media/
+
+# sync assets from blog.kristeraxel.com/media
+sudo rsync --delete --stats -tiprogl --progress /Users/theta/src/wp-mirror/ -e "ssh -i /Users/theta/src/aws/key/BlogKAkey.pem" ec2-user@107.22.165.75:/var/www/html/
