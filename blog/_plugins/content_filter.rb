50a9ca0d818c664e9b812a6ebd2df2e7d0db2479
Krister Axel <axel@theta>
2012-11-28 23:52:25 -0800

    audio filter supports multiples now | auto deploy 1354175545

--- blog/_plugins/content_filter.rb
+++ blog/_plugins/content_filter.rb
@@ -5,10 +5,13 @@ module Liquid
     
     def add_audio_tag(raw)
       filtered = ""
-      if raw.scan(/(<a.{1,}http:.{9,}.mp3.{1,}>)/)[0]
-        matchtag = raw.scan(/(<a.{1,}http:.{9,}.mp3.{1,}>)/)[0].to_s
-        filename = matchtag.scan(/<a.{1,}(http:.{9,}mp3)/)[0].to_s
-        filtered = raw.gsub!(matchtag, add_tag(filename))
+      if raw.scan(/(<a.href..http:..[a-zA-Z]{1,}[.]...{0,1}\/[\w\/-]{3,40}[.]mp3.{1,9}<\/a>)/)[0]
+        raw.scan(/(<a.href..http:..[a-zA-Z]{1,}[.]...{0,1}\/[\w\/-]{3,40}[.]mp3.{1,9}<\/a>)/).each do |this|
+          matchtag = this.to_s
+          filename = matchtag.scan(/<a.{1,}(http:.{9,}mp3)/)[0].to_s
+          filtered = raw.gsub!(matchtag, add_tag(filename))
+        end
+        
       else
         filtered = raw
       end
@@ -18,7 +21,7 @@ module Liquid
   private
   
     def add_tag(filename)
-      "<audio src='#{filename}' preload='auto' />"
+      "<p><audio src='#{filename}' preload='auto' /></p>"
     end
       
   end
