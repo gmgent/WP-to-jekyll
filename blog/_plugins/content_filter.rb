0b0877c29bc0f24b9c6acd493e708a1adb002bed
Krister Axel <axel@theta>
2012-11-28 23:59:15 -0800

    fixed audio filter tag to properly render multiple matches | auto deploy 1354175955

--- blog/_plugins/content_filter.rb
+++ blog/_plugins/content_filter.rb
@@ -5,8 +5,8 @@ module Liquid
     
     def add_audio_tag(raw)
       filtered = ""
-      if raw.scan(/(<a.href..http:..[a-zA-Z]{1,}[.]...{0,1}\/[\w\/-]{3,40}[.]mp3.{1,9}<\/a>)/)[0]
-        raw.scan(/(<a.href..http:..[a-zA-Z]{1,}[.]...{0,1}\/[\w\/-]{3,40}[.]mp3.{1,9}<\/a>)/).each do |this|
+      if raw.scan(/(<a.href..http:..[a-zA-Z]{1,12}[.].{0,12}...{0,1}\/[\w\/-]{3,40}[.]mp3.{1,9}<\/a>)/)[0]
+        raw.scan(/(<a.href..http:..[a-zA-Z]{1,12}[.].{0,12}...{0,1}\/[\w\/-]{3,40}[.]mp3.{1,9}<\/a>)/).each do |this|
           matchtag = this.to_s
           filename = matchtag.scan(/<a.{1,}(http:.{9,}mp3)/)[0].to_s
           filtered = raw.gsub!(matchtag, add_tag(filename))
