9a3b5592c5c79f9eb0edd68a14ba08b5a73cf917
Krister Axel <axel@theta>
2012-11-28 21:42:22 -0800

    last sync is added3 | auto deploy 1354167742

--- deploy_command.rb
+++ deploy_command.rb
@@ -6,6 +6,6 @@ sudo rsync --delete --stats -tiprogl --progress /Users/theta/src/jekyll/blog/_si
 #sync assets from kristeraxel.com/media
 sudo rsync --delete --stats -tiprogl --progress /Users/theta/src/kristeraxel.com/media/ -e "ssh -i /Users/theta/src/aws/key/BlogKAkey.pem" ec2-user@107.22.165.75:/var/www/kristeraxel.com/media/
 
-#We will try this one next. TRYING AGAIN
+#We will try this one next. TRYING AGAIN 2
 # sync assets from blog.kristeraxel.com/media
 sudo rsync --delete --stats -tiprogl --progress /Users/theta/src/wp-mirror/ -e "ssh -i /Users/theta/src/aws/key/BlogKAkey.pem" ec2-user@107.22.165.75:/var/www/html/
